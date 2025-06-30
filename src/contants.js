78b2b018db4a222655d25ac9baf23a266420a946
Syed Abdullah Ahmed <126831585+SyedAbdullahAhmed@users.noreply.github.com>
2025-06-30 12:44:30 -0400

    Update contants.js

--- src/contants.js
+++ src/contants.js
@@ -1,6 +1,6 @@
 // const BASE_URL = 'https://payment-wallet-system-backend.vercel.app'
 const BASE_URL = process.env.NODE_ENV !== 'development'
-  ? 'https://payment-wallet-system-backend.vercel.app'
+  ? 'https://vljen465ka.execute-api.ap-south-1.amazonaws.com'
   : 'http://localhost:3000';
 
 export { BASE_URL }
